52affbcd40fab9013cfda626251d9459b5db3f8e
Abhay Bansal <abhaybansal19000@gmail.com>
2024-11-24 15:27:43 +0530

    ADDED NAMES

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import InputBox from './components/InputBox'
 import useCurrencyInfo from './hooks/useCurrencyInfo'
+import { AnimatedTooltip } from "./components/animated-tooltip";
 
 
 function App() {
@@ -24,6 +25,38 @@ function App() {
   const convert = () => {
     setConvertedAmount(amount * currencyInfo[to])
   }
+
+  const people = [
+    {
+      id: 1,
+      name: "Abhay Bansal",
+      designation: "102217246",
+      image:
+        "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+    },
+    {
+      id: 2,
+      name: "Tamanna",
+      designation: "Product Manager",
+      image:
+        "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+      id: 3,
+      name: "Anushka",
+      designation: "Data Scientist",
+      image:
+        "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+      id: 4,
+      name: "Ankita",
+      designation: "UX Designer",
+      image:
+        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    },
+    
+  ];
 
   return (
     <div
@@ -74,6 +107,9 @@ function App() {
                         Convert {from.toUpperCase()} to {to.toUpperCase()}
                     </button>
                 </form>
+            </div>
+            <div className="mt-12 flex flex-row items-center justify-center mb-10 w-full">
+                <AnimatedTooltip items={people} />
             </div>
         </div>
     </div>
